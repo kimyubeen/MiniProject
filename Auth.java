6ead6ce5ca31c932c570e20e6722cadfdfaefd1f
kimyubeen <51874680+kimyubeen@users.noreply.github.com>
2019-06-27 16:27:47 +0900

    Add files via upload

---
+++ Auth.java
@@ -0,0 +1,32 @@
+package BookManagement;
+
+public class Auth {
+    private int memberId;
+    private String userId;
+    private String pw;
+    
+    public int getMemberId() {
+        return memberId;
+    }
+    
+    public void setMemberId(int memberId) {
+        this.memberId = memberId;
+    }
+    
+    public String getUserId() {
+        return userId;
+    }
+    
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    
+    public String getPw() {
+        return pw;
+    }
+    
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
+
+}
